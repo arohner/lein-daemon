8255ae876b345b4372fea05fb339d63b2eaf1f9d
Allen Rohner <arohner@gmail.com>
2010-07-15 16:08:52 -0500

    Bump the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-daemon "0.1"
+(defproject lein-daemon "0.2"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [commons-daemon "1.0.1"]]
