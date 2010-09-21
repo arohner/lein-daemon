e17b4fb40fecb77377ba107dc538c154af41c050
Allen Rohner <arohner@gmail.com>
2010-09-21 15:39:37 -0500

    Move clojure into a dev dependency so we don't specify our own clojure dependency at runtime

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject lein-daemon "0.2"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [commons-daemon "1.0.1"]]
+  :dependencies [[commons-daemon "1.0.1"]]
+  :dev-dependencies [[org.clojure/clojure "1.2.0"]]
   :aot [leiningen.daemon.daemonProxy])
