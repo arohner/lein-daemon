ba9eedf7caef5d6aa47dfac9f7656aca4c1d3fd7
Allen Rohner <arohner@gmail.com>
2013-02-27 11:59:06 -0600

    Ok, try eval-in :subprocess, setting source-path

--- test/leiningen/test_daemon.clj
+++ test/leiningen/test_daemon.clj
@@ -59,9 +59,13 @@
          (is (re-find #"lein daemon-starter foo" bash-cmd)))))))
 
 (deftest daemon-starter-finds-string-info
-  (starter/daemon-starter {:eval-in :leiningen
+  (starter/daemon-starter {:eval-in :subprocess
+                           :dependencies ['[org.clojure/clojure "1.4.0"]]
+                           :source-paths ["src"]
                            :daemon {"foo" {:ns "bogus.main"}}} "foo"))
 
 (deftest daemon-starter-finds-keyword-daemon
-  (starter/daemon-starter {:eval-in :leiningen
+  (starter/daemon-starter {:eval-in :subprocess
+                           :dependencies ['[org.clojure/clojure "1.4.0"]]
+                           :source-paths ["src"]
                            :daemon {:foo {:ns "bogus.main"}}} "foo"))
